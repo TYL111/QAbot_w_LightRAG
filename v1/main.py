601357c41c3874d403c561cc499eb9c988d32f09
TYL111 <92169885+TYL111@users.noreply.github.com>
2026-04-10 00:00:59 +0800

    Refactor upload function and improve file handling
    add upload history
    解決lightrag insert txt出錯照樣加入processed檔案問題

--- v1/main.py
+++ v1/main.py
@@ -7,10 +7,11 @@ from lightrag.utils import EmbeddingFunc
 from lightrag.llm.gemini import gemini_model_complete, gemini_embed
 from lightrag.llm.ollama import ollama_embed
 
+import json
 
-# Gemini API
+
+
 os.environ["GEMINI_API_KEY"] = "YOUR-API"
-
 
 # 初始化rag
 async def initial_rag():
@@ -27,14 +28,14 @@ async def initial_rag():
             system_prompt=system_prompt,
             history_messages=history_messages,
             api_key=os.getenv("GEMINI_API_KEY"),
-            model_name="gemini-2.5-flash",
+            model_name="gemini-2.5-flash-lite",
             **kwargs
         )
 
     rag = LightRAG(
         working_dir=WORKING_DIR,
         llm_model_func=llm_model_func,
-        llm_model_name="gemini-2.5-flash",
+        llm_model_name="gemini-2.5-flash-lite",
         embedding_func=EmbeddingFunc(
             embedding_dim=1024,  
             max_token_size=8192,
@@ -45,64 +46,86 @@ async def initial_rag():
 
     return rag
 
-def _get_txt_files(SOURCE):
-    if not os.path.exists(SOURCE):
-        return False
-    txts = [txt for txt in os.listdir(SOURCE) if txt.endswith(".txt")]
-    return txts
 
-def _get_pdf_files(SOURCE):
-    if not os.path.exists(SOURCE):
-        return False
-    pdfs = [pdf for pdf in os.listdir(SOURCE) if pdf.endswith(".pdf")]
-    return pdfs
+
 
 import pypdf
+
+
 async def upload(SOURCE,rag):
-    import shutil
-    PROCESSED_DIR = "./processed"
-    if not os.path.exists(PROCESSED_DIR):
-        os.mkdir(PROCESSED_DIR)
+
+
+    def _get_txt_files():
+        if not os.path.exists(SOURCE):
+            return False
+        txts = [txt for txt in os.listdir(SOURCE) if txt.endswith(".txt")]
+        return txts
+
+    def _get_pdf_files():
+        if not os.path.exists(SOURCE):
+            return False
+        pdfs = [pdf for pdf in os.listdir(SOURCE) if pdf.endswith(".pdf")]
+        return pdfs
+
+    def _load_or_create_history():
+        if not os.path.exists(os.path.join(SOURCE,"upload_history.json")):
+            init_data = {"processed_files":[]}
+            return init_data
+        else:
+            with open(os.path.join(SOURCE,"upload_history.json"),"r") as f:
+                return json.load(f)
+    upload_history = _load_or_create_history()
 
     #上傳.txt
-    txts = _get_txt_files(SOURCE)
+    txts = _get_txt_files()
     if txts:
         for txt in txts:
             print(f"正在處理{txt}")
+            if txt in upload_history["processed_files"]:
+                print(f"已處理過 {txt}")
+                continue # 跳過處理過
             file_path = os.path.join(SOURCE,txt)
             try:
-                #####################################################
-                ### LightRAG報錯不會跳exception 檔案照樣會移  //修
-                #####################################################
                 with open(file_path,"r",encoding="utf-8") as f:
-                    await rag.ainsert(f.read(),file_path = os.path.join(PROCESSED_DIR,txt))
-                shutil.move(file_path,os.path.join(PROCESSED_DIR,txt))
-                print(f"{txt}處理成功 移動至 {PROCESSED_DIR}")
+                    result = await rag.ainsert(f.read(),file_paths = [file_path])
+                upload_history["processed_files"].append(txt)
+                print(f"{txt}處理成功")
+                if not result:
+                    raise Exception(f"INSERT ERROR")
             except Exception as e:
-                print(f"處理檔案 {txt} 時發生錯誤: {e}")
-    
-    #上傳pdf
-    pdfs = _get_pdf_files(SOURCE)
-    if pdfs:
-        for pdf in pdfs:
-            print(f"正在處理{pdf}")
-            file_path = os.path.join(SOURCE,pdf)
-            try:
-                reader = pypdf.PdfReader(file_path)
-                reader
-            except Exception as e:
-                print(f"處理檔案 {pdf} 時發生錯誤: {e}")
+                print(f"{txt} {e}")
 
+
+    # 儲存history
+    with open(os.path.join(SOURCE,"upload_history.json"),"w",encoding="utf-8") as f:
+        json.dump(upload_history,f)
+
+
+    #上傳pdf
+    # pdfs = _get_pdf_files(SOURCE)
+    # if pdfs:
+    #     for pdf in pdfs:
+    #         print(f"正在處理{pdf}")
+    #         file_path = os.path.join(SOURCE,pdf)
+    #         try:
+    #             reader = pypdf.PdfReader(file_path)
+    #             reader
+    #         except Exception as e:
+    #             print(f"處理檔案 {pdf} 時發生錯誤: {e}")
+
+async def ask_q(q,rag):
+    response = await rag.aquery(
+        q, 
+        param=QueryParam(mode="hybrid", stream=False)
+    )
+    print(response)
             
 async def main():
     rag = await initial_rag()
     await upload("./source",rag)
+    # await ask_q("新北市經濟發展局負責人是誰？",rag)
 
-    # response = await rag.aquery(
-    #     "新北市經濟發展局負責人是誰？", 
-    #     param=QueryParam(mode="hybrid", stream=False)
-    # )
-    # print(response)
+
 if __name__ == "__main__":
 
     asyncio.run(main())
